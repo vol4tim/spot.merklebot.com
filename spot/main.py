8a9ba72cc5e21a1ae57f6bd181a7893a2740a72e
Arseniy <arseniypopugay@gmail.com>
2022-05-17 20:44:52 +0300

    calibration movement strategy change

--- spot/main.py
+++ spot/main.py
@@ -107,8 +107,8 @@ def empty_handler():
 def calibration_movement(sc):
     global max_width, max_height, coord_nodes
 
-    yaws = [(-1) ** (j % 2) * i / 10 for j in range(6) for i in range(-5, 6, 1)]
-    pitches = [i / 10 for i in range(-5, 6, 1) for j in range(6)]
+    yaws = [(-1) ** (j % 2) * i / 10 for j in range(8) for i in range(-5, 6, 1)]
+    pitches = [i / 10 for i in range(-5, 3, 1) for j in range(11)]
     rolls = [0] * len(yaws)
 
     calibration_result = {
