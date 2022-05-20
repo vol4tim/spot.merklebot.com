9e704c88a62976c8e22d958b5f03f88478bf3ecb
Arseniy <arseniypopugay@gmail.com>
2022-05-21 01:50:02 +0300

    graph clearing script

--- spot/current_graph_clearer.py
+++ spot/current_graph_clearer.py
@@ -1,5 +1,6 @@
 import bosdyn.client
 from bosdyn.client.graph_nav import GraphNavClient
+from bosdyn.client.recording import GraphNavRecordingServiceClient
 
 robot_ip = "192.168.50.3"
 username = "admin"
@@ -14,6 +15,8 @@ lease_client = robot.ensure_client('lease')
 lease = lease_client.take()
 lease_keep_alive = bosdyn.client.lease.LeaseKeepAlive(lease_client, must_acquire=True)
 
+recording_client = robot.ensure_client(GraphNavRecordingServiceClient.default_service_name)
 graph_nav_client = robot.ensure_client(GraphNavClient.default_service_name)
-graph_nav_client.stop_recording()
+
+recording_client.stop_recording()
 graph_nav_client.clear_graph()
