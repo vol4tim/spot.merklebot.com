0b8b2aed2c4277073f6b8a2aa4972d03b14a41c5
Arseniy <arseniypopugay@gmail.com>
2022-05-23 11:20:39 +0300

    Changed frontend title

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -7,7 +7,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'davos-frontend',
+    title: 'Play with Spot',
     htmlAttrs: {
       lang: 'en'
     },
