b5f0c466095ab42060b09a5bbd2ed316cb0adb4d
Arseniy <arseniypopugay@gmail.com>
2022-05-30 20:39:51 +0300

    add tester to control

--- spot/tester.py
+++ spot/tester.py
@@ -1,17 +1,7 @@
-import time
-from SpotController import SpotController
+from spot_controller import SpotController
 
-
-def empty_handler():
-    pass
-
-
-sc = SpotController("admin", "2zqa8dgw7lor", "192.168.50.3")
-
-sc.lease_control()
-sc.power_on_stand_up()
-time.sleep(3)
-sc.power_off_sit_down()
-
-sc.move_to_draw(start_drawing_trigger_handler=empty_handler, end_drawing_trigger_handler=empty_handler,
-                xx=[0, 400, 400, 0, 0], yy=[0, 0, 300, 300, 0])
+with SpotController("admin", "2zqa8dgw7lor", "192.168.50.3") as sc:
+    yaws = [(-1) ** (j % 2) * i / 10 for j in range(8) for i in range(-5, 6, 1)]
+    pitches = [i / 10 for i in range(-5, 3, 1) for j in range(11)]
+    rolls = [0] * len(yaws)
+    sc.move_head_in_points(yaws, pitches, rolls)
