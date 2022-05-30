e67164bc5c19867f5b5a39ca1540722bea173ee2
Arseniy <arseniypopugay@gmail.com>
2022-05-30 20:41:48 +0300

    add tester to control

--- spot/tester.py
+++ spot/tester.py
@@ -1,4 +1,14 @@
 from spot_controller import SpotController
+import json
+
+coord_nodes = json.load(open("calibration_data_final.json")) if os.path.exists("calibration_data_final.json") else {
+    "x": [0, 0, 400, 400],
+    "y": [0, 300, 0, 300],
+    "yaw": [-0.4, -0.4, 0.4, 0.4],
+    "pitch": [-0.4, 0.4, -0.4, 0.4],
+    "max_width": 400,
+    "max_height": 300
+}
 
 with SpotController("admin", "2zqa8dgw7lor", "192.168.50.3") as sc:
     yaws = [(-1) ** (j % 2) * i / 10 for j in range(8) for i in range(-5, 6, 1)]
