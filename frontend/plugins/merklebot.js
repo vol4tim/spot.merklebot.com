bdad2ac1625f9a8da32309775c9f8afa2b93ff4f
Alisher A. Khassanov <a.khssnv@gmail.com>
2022-06-07 18:02:43 +0300

    Robonomics launch trace reading

---
+++ frontend/plugins/merklebot.js
@@ -0,0 +1,22 @@
+import axios from 'axios'
+
+const client = axios.create({
+  baseURL: 'https://api.merklebot.com'
+})
+
+/**
+ * Read robonomics launch traces from backend by sender and launch tx nonce.
+ * Check docs at https://api.merklebot.com/docs#/Robonomics-launch-traces/get_all_robonomics_launch_traces_get.
+ * @param {string} [sender] - Sender's SS58 account address in Robonomics format.
+ * @param {number} [nonce] - Launch tx nonce.
+ * @returns {?(Object|Array)} - All traces if no argument provided, traces related to the sender if specified, or a specific trace if launch tx nonce provided.
+ */
+export const readRobonomicsLaunchTracesBySender = async (sender, nonce) => {
+  const resp = await client.get('robonomics-launch-traces', {
+    params: {
+      sender,
+      nonce
+    }
+  })
+  return resp.data
+}
