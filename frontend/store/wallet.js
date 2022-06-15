528173b43a5cc41fb8e190e0c03e261e036262bf
Alisher A. Khassanov <a.khssnv@gmail.com>
2022-06-15 17:53:31 +0300

    Frontend: fix balance update

--- frontend/store/wallet.js
+++ frontend/store/wallet.js
@@ -3,8 +3,7 @@ import { readTicketsByCustomer } from '../plugins/merklebot'
 import {
   getAccounts,
   setActiveAccount,
-  subscribeToBalanceUpdates,
-  getInstance as getRobonomics
+  subscribeToBalanceUpdates
 
 } from '@/plugins/robonomics'
 
@@ -37,13 +36,11 @@ export const useWallet = defineStore('wallet', {
       this.selectedAccount.account = account
       setActiveAccount(account.address)
 
-      subscribeToBalanceUpdates(this.selectedAccount.account.address, (r) => {
-        getRobonomics().then((robonomics) => {
-          const balance = r.free.sub(r.feeFrozen)
-          this.selectedAccount.balanceRaw = balance
-          this.selectedAccount.balanceFormatted = (balance * 10 ** -9).toFixed(4) + ' XRT'
-          readTicketsByCustomer(account.address).then((tickets) => { this.selectedAccount.tickets = tickets })
-        })
+      subscribeToBalanceUpdates(this.selectedAccount.account.address, ({ free, feeFrozen }) => {
+        const balance = free.sub(feeFrozen)
+        this.selectedAccount.balanceRaw = balance
+        this.selectedAccount.balanceFormatted = (balance * 10 ** -9).toFixed(4) + ' XRT'
+        readTicketsByCustomer(account.address).then((tickets) => { this.selectedAccount.tickets = tickets })
       })
     }
   }
