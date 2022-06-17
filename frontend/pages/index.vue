68598d02b76ec1f2dcf27e390c78970c7d599079
Alisher A. Khassanov <a.khssnv@gmail.com>
2022-06-17 15:29:03 +0300

    Frontend: open on a section specified by hash

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -36,10 +36,11 @@
         <div class="overflow-x-hidden overflow-y-auto h-screen snap-y snap-mandatory">
           <div class="screen flex items-center snap-always snap-center scroll-my-10">
             <div class="dark:bg-gray-700 border-2 border-gray-800 dark:border-white mx-6 my-12 px-6 py-8 align-middle">
-              <h3 id="1" class="max-w-md text-xl md:text-2xl font-bold mx-6 dark:text-white text-gray-800">
-                1. User Ownership of Data
-              </h3>
-
+              <a id="anchor-to-1" href="#1">
+                <h3 id="anchor-to-1" class="max-w-md text-xl md:text-2xl font-bold mx-6 dark:text-white text-gray-800">
+                  1. User Ownership of Data
+                </h3>
+              </a>
               <p class="text-md my-2 dark:text-white mx-6">
                 Identity and access rights are managed using the cryptographic key from your Web3 wallet. This way we make sure that you own the data generated during your interaction with the robot. And for enterprise cases we cryptographically verify the whole chain of custody for machine data within and accross organizations.
               </p>
@@ -57,9 +58,11 @@
 
           <div class="screen flex items-center snap-always snap-center scroll-my-10">
             <div class="dark:bg-gray-700 border-2 border-gray-800 dark:border-white mx-6 my-12 px-6 py-8">
-              <h3 id="2" class="max-w-md text-xl md:text-2xl font-bold mx-6 dark:text-white text-gray-800">
-                2. Robots Transfer Value
-              </h3>
+              <a id="anchor-to-2" href="#2">
+                <h3 id="2" class="max-w-md text-xl md:text-2xl font-bold mx-6 dark:text-white text-gray-800">
+                  2. Robots Transfer Value
+                </h3>
+              </a>
               <p class="text-md my-2 dark:text-white mx-6">
                 Billing for robot’s work can happen in both US dollars and cryptocurrencies - money for robots. While robots today can already navigate our complex world, perform inspections and even interact with people - they still couldn’t participate in our economy directly. From fun demos like this one to data-driven equipment leasing: now robots can transact in fully autonomous supply chains.
               </p>
@@ -70,9 +73,11 @@
 
           <div class="screen flex items-center snap-always snap-center scroll-my-10">
             <div class="dark:bg-gray-700 border-2 border-gray-800 dark:border-white mx-6 my-12 px-6 py-8">
-              <h3 id="3" class="max-w-md text-xl md:text-2xl font-bold mx-6 dark:text-white text-gray-800">
-                3. Secure Access to Machines
-              </h3>
+              <a id="anchor-to-3" href="#3">
+                <h3 id="3" class="max-w-md text-xl md:text-2xl font-bold mx-6 dark:text-white text-gray-800">
+                  3. Secure Access to Machines
+                </h3>
+              </a>
               <p class="text-md my-2 dark:text-white mx-6">
                 Teleoperation session is authorized by the key in your wallet and the data can be encrypted with it too. You will now send a transaction directly to the robot and it will be recorded in Robonomics blockchain within Polkadot ecosystem. This way technical command and value transfer are tied together and verified to ensure resiliency of large scale cyber-physical systems.
               </p>
@@ -89,9 +94,11 @@
 
           <div class="screen flex items-center snap-always snap-center scroll-my-10">
             <div class="dark:bg-gray-700 border-2 border-gray-800 dark:border-white mx-6 my-12 px-6 py-8">
-              <h3 id="4" class="w-full text-xl md:text-2xl font-bold mx-6 dark:text-white text-gray-800">
-                4. Decentralized Storage and Reporting
-              </h3>
+              <a id="anchor-to-4" href="#4">
+                <h3 id="4" class="w-full text-xl md:text-2xl font-bold mx-6 dark:text-white text-gray-800">
+                  4. Decentralized Storage and Reporting
+                </h3>
+              </a>
               <p class="text-md my-2 dark:text-white mx-6">
                 Most secure and affordable way to store large amounts of machine data. Large data sets that robots generate are recorded using Filecoin Network. It uses cryptographic storage proofs to verify data is being stored correctly and securely over time in decentralized network of storage providers. All with signifficantly lower storage costs compared to traditional clouds.
               </p>
@@ -104,9 +111,11 @@
 
           <div class="screen flex items-center snap-always snap-center scroll-my-10 mb-32">
             <div class="dark:bg-gray-700 border-2 border-gray-800 dark:border-white mx-6 my-12 px-6 py-8">
-              <h3 id="5" class="w-full text-xl md:text-2xl font-bold mx-6 dark:text-white text-gray-800">
-                5. Digital Economies Powering Physical World
-              </h3>
+              <a id="anchor-to-5" href="#5">
+                <h3 id="anchor-to-5" class="w-full text-xl md:text-2xl font-bold mx-6 dark:text-white text-gray-800">
+                  5. Digital Economies Powering Physical World
+                </h3>
+              </a>
               <p class="text-md my-2 dark:text-white mx-6">
                 With the rise of cryptocurrencies adoption new digital market flourished and they present a number of opportunities to reduce costs, drive additional revenue streams and partnerships.
               </p>
@@ -165,7 +174,7 @@
 </template>
 
 <script>
-import { defineComponent, onMounted, ref } from '@nuxtjs/composition-api'
+import { defineComponent, onMounted, ref, useRoute } from '@nuxtjs/composition-api'
 import { useRobot } from '../store/robot'
 import { useWallet } from '../store/wallet'
 
@@ -192,6 +201,16 @@ export default defineComponent({
       window.addEventListener('resize', () => {
         screenSize.value = { w: window.innerWidth, h: window.innerHeight }
       })
+
+      // Scroll to 3-rd section after a Stripe purchase redirect.
+      // Required while default browser behavior does not scroll to anchor inside a flexbox
+      // as well as Element.scroll. Good for us anchor.click works.
+      const route = useRoute()
+      if (route.value.hash) {
+        setTimeout(() => {
+          document.getElementById(`anchor-to-${route.value.hash.slice(1)}`).click()
+        }, 300)
+      }
     })
 
     const minScreenSize = { w: 1000, h: 400 }
