9c1eed66b353ee354fc260fd23bd24aec1eb53d2
alex <alexander.smehnov@gmail.com>
2022-10-24 17:17:39 +0300

    spot: make helloween card

--- spot/video_recorder.py
+++ spot/video_recorder.py
@@ -4,6 +4,37 @@ import cv2
 import urllib.request
 import numpy as np
 
+def make_helloween_card(drawing):
+  template = cv2.imread("NFT_SPOT_Helloween.jpeg")
+  t_w, t_h, _ = template.shape
+  mask_template = np.ones((t_h, t_w))*255
+
+  drawing = cv2.blur(drawing, (3,3))
+  drawing_hsv = cv2.cvtColor(drawing, cv2.COLOR_BGR2HSV)
+  mask = cv2.inRange(drawing_hsv, np.array([0,50,50]), np.array([10,255,255]))
+
+  x,y,w,h = cv2.boundingRect(mask)
+  cropped_mask = mask[y:y+h, x:x+w]
+  cropped_mask = cv2.bitwise_not(cropped_mask)
+
+  template_area_x = 1700
+  template_area_y = 800
+  template_area_w = 2200
+  template_area_h = 1500
+
+  if w>h:
+    resized_mask = cv2.resize(cropped_mask, (template_area_w, int(template_area_w/w*h)), interpolation = cv2.INTER_AREA)
+    resized_mask = cv2.blur(resized_mask, (13,13))
+    mask_template[template_area_y:template_area_y+int(template_area_w/w*h), template_area_x:template_area_x+template_area_w] = resized_mask[:,:]
+  else:
+    resized_mask = cv2.resize(cropped_mask, (int(template_area_h/h*w), template_area_h), interpolation = cv2.INTER_AREA)
+    resized_mask = cv2.blur(resized_mask, (13,13))
+    mask_template[template_area_y:template_area_y+template_area_h, template_area_x:template_area_x+int(template_area_h/h*w)] = resized_mask[:,:]
+
+  mask_template = (mask_template>0)*255
+  mask_template = mask_template.astype('uint8')
+
+  return cv2.bitwise_and(template, template, mask=mask_template)
 
 def start_record(video_url, output_path, last_im_file, last_drawing_file, helloween_drawing_file):
     stream = cv2.VideoCapture(video_url)
@@ -32,7 +63,8 @@ def start_record(video_url, output_path, last_im_file, last_drawing_file, hellow
             arr = np.asarray(bytearray(req.read()), dtype=np.uint8)
             last_drawing = cv2.imdecode(arr, -1)
             cv2.imwrite(last_drawing_file, last_drawing, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
-
+            helloween_img = make_helloween_card(last_drawing)
+            cv2.imwrite(helloween_drawing_file, helloween_img, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
 
         stream.release()
         out.release()
