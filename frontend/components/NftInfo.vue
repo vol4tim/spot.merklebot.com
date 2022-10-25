025aa4c1ff8c5cecdf5d1d0b9b5b67f8c5be8467
Alisher A. Khassanov <a.khssnv@gmail.com>
2022-10-25 18:58:39 +0600

    Frontend: add NFT order status

--- frontend/components/NftInfo.vue
+++ frontend/components/NftInfo.vue
@@ -1,6 +1,14 @@
 <template>
   <div hidden>
     NFT Data: {{ nftOrderInfo }}
+    <p class="text-md mt-2 text-white">
+      NFT minting status:
+      <span v-if="nftOrderInfo" class="text-yellow-500">{{ nftOrderInfo.status }}</span>
+      <Spinner v-else />
+    </p>
+    <p v-if="['minted', 'delivered'].includes(nftOrderInfo.status)" class="text-md mt-2 text-white">
+      Find your NFT at:
+    </p>
   </div>
 </template>
 
@@ -15,8 +23,13 @@ export default defineComponent({
     }
   },
   setup (props) {
-    const nftOrderInfo = props.data
-    return { nftOrderInfo }
+    const nftOrderInfo = props.info
+
+    const makeNftUrl = (collectionId, symbol, mintBlock, serialNumber) => {
+      return `https://singular.app/collectibles/kusama/${collectionId}-${symbol}/${mintBlock}-${collectionId}-${symbol}-${symbol}-${serialNumber}`
+    }
+
+    return { nftOrderInfo, makeNftUrl }
   }
 })
 </script>
