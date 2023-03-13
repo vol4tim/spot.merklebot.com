f47fbb6b47c28feb3ccca8695388f2f68b8d44a2
alex <alexander.smehnov@gmail.com>
2023-03-14 01:43:17 +0300

    videoserver: pass image to fake webcam

--- videoserver/main.py
+++ videoserver/main.py
@@ -159,7 +159,7 @@ def run_camera(im, state):
     camera_control = CameraControl()
     camera_control.set_zoom(173)
 
-    # fake_camera = pyfakewebcam.FakeWebcam(FAKE_CAMERA, 1280, 720)
+    fake_camera = pyfakewebcam.FakeWebcam(FAKE_CAMERA, 1280, 720)
 
     while True:
         (grabbed, frame) = stream.read()
@@ -218,7 +218,7 @@ def run_camera(im, state):
         blackboard_blended = cv2.addWeighted(blackboard_blended, 1, blackboard, 1, 0)
         im_rgb = cv2.cvtColor(blackboard_blended, cv2.COLOR_BGR2RGB)
 
-        # fake_camera.schedule_frame(im_rgb)
+        fake_camera.schedule_frame(im_rgb)
         im[0] = blackboard_blended
         im[1] = obj
         im[2] = blackboard
