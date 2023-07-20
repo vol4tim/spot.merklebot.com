d9c5abf9684276673c17a34df9a0705289c049bd
vol4tim <vol4tim@users.noreply.github.com>
2023-07-20 19:29:11 +0000

    deploy: 7bbfcd485fb571e593034cc12fbd8c4efefb5b8c

---
+++ _nuxt/35b436e.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[67],{1877:function(n,w){}}]);
---
+++ _nuxt/d5e37f2.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[22],{1192:function(t,n,e){"use strict";e.r(n);var l=e(25),component=Object(l.a)({},(function(){var t=this,n=t.$createElement,e=t._self._c||n;return e("div",{staticClass:"m-4 min-h-200 p-2"},[e("div",{staticClass:"grid grid-cols-2 gap-4"},[t._t("default")],2)])}),[],!1,null,null,null);n.default=component.exports}}]);
